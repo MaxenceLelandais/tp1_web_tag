c43d1cab1b0d86a1a37adf5ce7fb870580b75795
SabrinaAntinozzi <1285761@cstjean.qc.ca>
2022-10-17 12:37:50 -0400

    Fin score et début leaderboard

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -2,6 +2,11 @@
 
 export default function Leaderboard() {
     const wrapper = $('<div></div>').addClass('wrapper');
-    const section = $('<div></div>').addClass('section');
-    
+    const leaderboardSection = $('<div></div>').addClass('leaderboardSection');
+    const leaderboardtabs = $('<div></div>').addClass('leaderboardtabs');
+    const leaderboard = $('<div></div>').addClass('leaderboard');
+    const ulli = $('<ul></ul>');
+    const lien = $('<li></li>');
+    const lien1 = $('<li></li>');
+    const lien2 = $('<li></li>');
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { canvas, refreshCanvas } from "./components/canvas.js";
-import { formGenerationMap } from "./components/formGenerationMap.js";
+import { pageNewGame } from "./components/pageNewGame.js";
 
-document.body.appendChild(formGenerationMap());
-document.body.appendChild(canvas());
-refreshCanvas(400, document.getElementById('sizeMap').value * 2, document.getElementById('sizeMap').value, true);
+$('body').append(pageNewGame());
