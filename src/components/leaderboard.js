4ba40b12561d7eb035031daffb7872b485761843
SabrinaAntinozzi <1285761@cstjean.qc.ca>
2022-10-19 09:30:46 -0400

    Organisation du code

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -1,5 +1,6 @@
+import { restoreScore } from "../model/score.js";
 
-export default function Leaderboard() {
+export function Leaderboard() {
     const leaderboard = $(`
         <div class="wrapper">
             <div class="leaderboardSection">
@@ -13,8 +14,26 @@ export default function Leaderboard() {
                     </div>
                 </div>
             </div>
-        </div>   
+        </div>
     `);
 
-    $('body').append(leaderboard);
+    leaderboard.append(restoreScore);
+
+    return leaderboard;
 }
+
+/*
+    <div class="wrapper">
+        <div class="leaderboardSection">
+            <div class="leaderboardtabs">
+                <div class="leaderboard">
+                    <ul>
+                        <li class="active">Score</li>
+                        <li>Classe</li>
+                        <li>Map</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import { pageNewGame } from "./components/pageNewGame.js";
+//import { pageNewGame } from "./components/pageNewGame.js";
+import { Leaderboard } from "./components/leaderboard.js";
 
-$('body').append(pageNewGame());
+
+$('body').append(Leaderboard()); //avec le score
+//$('body').append(pageNewGame());
+//directement dans le jeu
