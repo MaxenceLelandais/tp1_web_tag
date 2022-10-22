65337c3ae5615af68e61de3608ab00f186f77f22
Maxence Lelandais <maxence.lelandais@free.fr>
2022-10-22 18:32:37 -0400

    Rajout

---
+++ src/components/leaderboard.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+import {restoreScore} from '../model/score.js';
+
+export function Leaderboard() {
+  const leaderboard = $(`
+        <div class="wrapper">
+            <div class="leaderboardSection">
+                <div class="leaderboardtabs">
+                    <div class="leaderboard">
+                        <ul>
+                            <li class="active">Score</li>
+                            <li>Classe</li>
+                            <li>Map</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+
+  leaderboard.append(restoreScore);
+
+  return leaderboard;
+}
